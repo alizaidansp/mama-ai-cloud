a6338781b25cbc3139c165a0c2277a3006c364ef
Ali Zaidan <zaidanali028@gmail.com>
2025-10-29 12:35:32 +0000

    Initial Terraform configuration

---
+++ main.tf
@@ -0,0 +1,14 @@
+# main.tf
+terraform {
+  required_version = ">= 1.0"
+}
+
+# Simple local resource for testing
+resource "local_file" "test_file" {
+  content  = "Hello from Mama AI!"
+  filename = "test.txt"
+}
+
+output "file_content" {
+  value = local_file.test_file.content
+}
